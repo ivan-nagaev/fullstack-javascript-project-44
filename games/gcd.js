1ce199f36a944c2594dc4715926264e26606a02f
Ivan Nagaev <ivan.nagaev@internet.ru>
2024-04-24 08:12:29 +0300

    add brain-gcs & gcd.js

---
+++ games/gcd.js
@@ -0,0 +1,33 @@
+import readLineSync from 'readline-sync';
+import { greetings, randomNum, gcd } from '../src/index.js';
+
+const userName = greetings('gcd');
+
+export default () => {
+  let count = 0;
+  let game = true;
+
+  while (count < 3 && game) {
+    const num1 = randomNum();
+    const num2 = randomNum();
+    const divisor = gcd(num1, num2);
+
+    // calc divisor
+    console.log(`Question: ${num1} ${num2}`);
+    const userAnswer = Number(readLineSync.question('Your answer: '));
+
+    if (userAnswer === divisor) {
+      console.log('Correct!');
+    } else {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${divisor}'.`
+      );
+      game = false;
+      return console.log(`Let's try again, ${userName}!`);
+    }
+
+    count += 1;
+  }
+
+  return console.log(`Congratulations, ${userName}!`);
+};
